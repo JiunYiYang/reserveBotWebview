3dc9c450e3e5408e05e8b9de981f5107a2544569
ntu_ror_ubuntu <rubyyang0615@gmail.com>
2017-10-26 01:38:39 +0800

    table view sorting

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,6 +8,8 @@ import { Router, ActivatedRoute, Params, NavigationCancel } from '@angular/route
 import { Reserve } from './../shared/reserve';
 import { RESERVES } from './../shared/reserves';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -20,7 +22,10 @@ export class FormComponent implements OnInit {
 
   minDate = new Date(2017, 9, 12);
   maxDate = new Date(2017, 9, 19);
+	
+	maxUsers = 6;
 
+	availableTime = []
 
   hours = [
     {value: '12:00', viewValue: '12:00'},
@@ -39,12 +44,12 @@ export class FormComponent implements OnInit {
   ];
 
   users = [
-    {value: 1, viewValue: '1'},
-    {value: 2, viewValue: '2'},
-    {value: 3, viewValue: '3'},
-    {value: 4, viewValue: '4'},
-    {value: 5, viewValue: '5'},
-    {value: 6, viewValue: '6'}
+    {value: 1, viewValue: 1},
+    {value: 2, viewValue: 2},
+    {value: 3, viewValue: 3},
+    {value: 4, viewValue: 4},
+    {value: 5, viewValue: 5},
+    {value: 6, viewValue: 6}
   ];
 
   private token;
@@ -63,18 +68,39 @@ export class FormComponent implements OnInit {
     //     let id = params['id'];
     //     console.log(id);
     //   });
-
       this.reserve = {
         tokenid: '',
         name: '',
         phone: '',
         date: '',
         time: '',
+        dateTime: '',
         users: 0
       };
   }
 
-  ngOnInit() {
+  dateFilter = (d: Date): boolean => {
+		return this.availableTime.find( e => d.getDate() == e.time.getDate() );
+  } 
+
+	onDateChange() {
+		this.hours = this.availableTime.filter( e => {
+			return e.time.getDate() == (new Date(this.reserve.date)).getDate();
+		}).map((i) => {
+			let m_time = moment(i.time).format("HH:mm");
+			return {value: m_time, viewValue: m_time};
+		});
+	}
+
+	onTimeChange() {
+		let tmp = this.availableTime.find( e => this.m_getDateTime().toDate().getTime() == e.time.getTime() );
+		this.users = [];
+		for (let i = 1; i <= (this.maxUsers > tmp.available ? tmp.available : this.maxUsers); i++) {
+			this.users.push({value: i, viewValue: i});
+		}
+	}
+
+	ngOnInit() {
     this.route
         .queryParams
         .subscribe((params: Params) => {
@@ -82,14 +108,25 @@ export class FormComponent implements OnInit {
             console.log(token);
             this.reserve.tokenid = token;
         })
-
+    this.http.get(`https://restaurant.mouther.one/get-available-time?tokenid=${this.reserve.tokenid}`).subscribe(res => {
+			this.availableTime = res.json().map( i => { i.time = new Date(i.time); return i; } );
+      console.log(this.availableTime);
+    });
   }
+
+	m_getDateTime() {
+    let time = this.reserve.time.split(":");
+		return moment(this.reserve.date).add({hours: parseInt(time[0]), minutes: parseInt(time[1])});
+	}
 
   onSubmit() {
     console.log("Reserve: ", this.reserve);
     console.log(this.token);
     console.log(this.reserve.tokenid);
+    this.reserve.dateTime = this.m_getDateTime().format("YYYY-MM-DD HH:mm:ss");
     this.reserves.push(this.reserve);
-    this.http.post('https://restaurant.mouther.one/booking', JSON.stringify(this.reserve)).subscribe(res => { console.log(res); });
+    this.http.post('https://restaurant.mouther.one/booking', this.reserve).subscribe(res => {
+      console.log(res.json());
+    });
   }
 }
